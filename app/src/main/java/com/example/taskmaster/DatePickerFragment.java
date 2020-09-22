4a8738617344ada6902166ec3772f11ad9bc1f19
jaggerabney <jagger.abney@gmail.com>
2020-09-21 23:13:47 -0700

    added a datepickerdialog fragment when calendar is selected

---
+++ app/src/main/java/com/example/taskmaster/DatePickerFragment.java
@@ -0,0 +1,38 @@
+package com.example.taskmaster;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DatePickerFragment extends AppCompatDialogFragment implements DatePickerDialog.OnDateSetListener {
+    private static final String TAG = "DatePickerFragment";
+    final Calendar calendar = Calendar.getInstance();
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        String selectedDate = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH).format(calendar.getTime());
+        Toast.makeText(getActivity(), selectedDate, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), DatePickerFragment.this, year, month, day);
+    }
+}
--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -344,6 +344,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.action_calendar:
+                DatePickerFragment dpf = new DatePickerFragment();
+                dpf.show(getSupportFragmentManager(), "select_date");
+                return true;
             case R.id.action_settings:
                 Intent intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
